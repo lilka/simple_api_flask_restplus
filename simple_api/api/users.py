a622254eafd225782491ed91b0d757689da88043
lilka <lili.lomnicka@gmail.com>
2020-02-17 17:41:04 +0100

    Get all users is working

--- simple_api/api/users.py
+++ simple_api/api/users.py
@@ -1,13 +1,13 @@
 import logging
 
-from flask import request, jsonify
+from flask import request
 from flask_restplus import Resource
-import json
+
 from simple_api.api.buisness import create_user, delete_user, update_user
 from simple_api.api.restplus import api
 from simple_api.database.models import User
-from simple_api.parsers import pagination_arguments
-from simple_api.serializers import user, users
+
+from simple_api.serializers import user
 
 log = logging.getLogger(__name__)
 
@@ -17,24 +17,13 @@ ns = api.namespace('user', description='Operations related to user')
 @ns.route('/')
 class UsersCollection(Resource):
 
-
-    @api.marshal_with(users)
+    @api.marshal_with(user)
     def get(self):
         """
         Returns list of users.
         # """
-        # args = pagination_arguments.parse_args(request)
-        # page = args.get('page', 1)
-        # per_page = args.get('per_page', 10)
-        #
-        # users_query = User.query
-        # users_page = users_query.paginate(page, per_page, error_out=False)
-        # print(User.query.all())
-        # print(users_query)
 
-
-        users = User.query.all()
-        return json.dumps(User.serialize_list(users))
+        return User.query.all()
 
     @api.expect(user)
     def post(self):
@@ -73,5 +62,3 @@ class UserItem(Resource):
         """
         delete_user(id)
         return None, 204
-
-
--- simple_api/serializers.py
+++ simple_api/serializers.py
@@ -9,15 +9,4 @@ user = api.model('User', {
 
 })
 
-pagination = api.model('A page of results', {
-    'page': fields.Integer(description='Number of this page of results'),
-    'pages': fields.Integer(description='Total number of pages of results'),
-    'per_page': fields.Integer(description='Number of items per page of results'),
-    'total': fields.Integer(description='Total number of results'),
-})
-
-users = api.model('Users',{
-    'users': fields.List(fields.Nested(user))
-})
-
 
